36795afe05f3c9bc0970924542d60088910f107b
elbouhatiahlam53-bit <elbouhatiahlam53@gmail.com>
2026-04-07 10:33:34 +0100

    first commit creation

---
+++ api/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h1>Ahlam El Bouhati</h1>
+    </header>
+    <?php
+    $groupe="dev 101";
+    $plt="vercel";
+    echo("mon premier site");
+    ?>
+
+</body>
+</html>
